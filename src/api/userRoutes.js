205117610272093a233822690794d1dab5c78664
Roshmie <you@example.com>
2025-04-18 13:36:14 +0530

    Update_userroutes

---
+++ src/api/userRoutes.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { BASEURL } from '@env';
+
+const axiosUserInstance = axios.create({
+  baseURL: `${BASEURL}/users`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getUserById = async (id) => {
+    try {
+    const userResponse = await axiosUserInstance.get(`/getUserbyId/${id}`);
+    console.log('userResponse :',userResponse.data);
+      return userResponse.data;
+  } catch (error) {
+    console.log("error :", error.userResponse?.data?.error);
+  }
+};
+
+export const updateUser = async (id, updatedData) => {
+    try {
+    const userResponse = await axiosUserInstance.put(`/updateUser/${id}`,updatedData);
+    console.log('userResponse :',userResponse.data);
+      return userResponse.data;
+  } catch (error) {
+    console.log("error :", error.userResponse?.data?.error);
+  }
+};
+
+/* export const getUsers = async () => {
+    try {
+    const userResponse = await axiosUserInstance.get(`/getUser`);
+    console.log('userResponse :',userResponse.data);
+      return userResponse.data;
+  } catch (error) {
+    console.log("error :", error.userResponse?.data?.error);
+  }
+}; */
